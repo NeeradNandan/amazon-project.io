fdcbdb612ba7613b2c179acb517ee29ae6470c85
Neerad Nandan <nrdnandan1705@gmail.com>
2024-12-27 11:46:42 +0530

    Combine HTML & JavaScript:
    - Add a script tag to load checkout.js
    - Write an entire reusable HTML in checkout.js
    
    Use money formatter:
    
    - Create a new JS file to be used in all JS modules
    
    Update Cart Quantity:
    
    - Use localStorage to update Cart immediately in all the routes
    
    Update & Delete functionality:
    
    - Implement logic for delete and updating the cart
    - Implement logic for update, input, and save
    - Immediately display quantity for each product
    - Implement both click and Enter logic.

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,16 @@
-export const cart = [];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if(cart.length === 0) {
+    cart = [
+        {
+            id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+            quantity: 2
+        },
+        {
+            id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+            quantity: 1
+        }];
+}
 
 export function addToCart(productId, productQuantity) {
     let matchingItem;
@@ -17,4 +29,30 @@ export function addToCart(productId, productQuantity) {
             quantity: productQuantity
         });
     }
+
+    saveToCart();
+}
+
+export function deleteFromCart (productId) {
+    cart.forEach((cartItem, index) => {
+        if (cartItem.id === productId) {
+           // console.log(cart);
+            cart.splice(index, 1);
+        }
+    })
+
+    saveToCart();
+}
+
+export function saveToCart() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+export function displayCartQuantity(cart) {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+        cartQuantity += cartItem.quantity;
+    })
+
+    return cartQuantity;
 }
---
+++ scripts/checkout.js
@@ -0,0 +1,182 @@
+import {addToCart, cart, deleteFromCart, displayCartQuantity, saveToCart} from '../data/cart.js';
+import { products } from '../data/products.js';
+import { formatCurrency} from "./utils/money.js";
+
+let cartSummaryHTML = '';
+
+cart.forEach((cartItem) => {
+    const productId = cartItem.id;
+    const productQuantity = cartItem.quantity;
+
+    let matchingProduct;
+
+    products.forEach((product) => {
+        if (productId === product.id) {
+            matchingProduct = product;
+        }
+    });
+
+
+    cartSummaryHTML += `
+    <div class="cart-item-container-${matchingProduct.id}">
+            <div class="delivery-date">
+              Delivery date: Tuesday, June 21
+            </div>
+
+            <div class="cart-item-details-grid">
+              <img class="product-image"
+                src="${matchingProduct.image}">
+
+              <div class="cart-item-details">
+                <div class="product-name">
+                  ${matchingProduct.name}
+                </div>
+                <div class="product-price">
+                  $${formatCurrency(matchingProduct.priceCents)}
+                </div>
+                <div class="product-quantity">
+                  <span>
+                    Quantity: <span class="quantity-label-${matchingProduct.id}">${quantityLabel(matchingProduct.id)}</span>
+                  </span>
+                  <span 
+                  data-product-id="${matchingProduct.id}"
+                  class="update-quantity-link link-primary">
+                    Update
+                  </span>
+                  <input type="number" min="0" max="1000" class="quantity-input-${matchingProduct.id} input-frame">
+                  <span
+                  class="save-quantity-link-${matchingProduct.id} save-link-primary link-primary">
+                    Save
+                  </span>
+ 
+                  <span class="delete-quantity-link link-primary"
+                  data-product-id="${matchingProduct.id}"
+                  >
+                    Delete
+                  </span>
+                </div>
+              </div>
+
+              <div class="delivery-options">
+                <div class="delivery-options-title">
+                  Choose a delivery option:
+                </div>
+                <div class="delivery-option">
+                  <input type="radio" checked
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Tuesday, June 21
+                    </div>
+                    <div class="delivery-option-price">
+                      FREE Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Wednesday, June 15
+                    </div>
+                    <div class="delivery-option-price">
+                      $4.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+                <div class="delivery-option">
+                  <input type="radio"
+                    class="delivery-option-input"
+                    name="delivery-option-${matchingProduct.id}">
+                  <div>
+                    <div class="delivery-option-date">
+                      Monday, June 13
+                    </div>
+                    <div class="delivery-option-price">
+                      $9.99 - Shipping
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+    `
+});
+
+document.querySelector('.order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.delete-quantity-link')
+    .forEach((linkElement) => {
+    linkElement
+        .addEventListener('click', () => {
+            const productId = linkElement.dataset.productId;
+
+            deleteFromCart(productId);
+
+
+            document.querySelector(`.cart-item-container-${productId}`).remove();
+            document.querySelector('.checkout-header-middle-section').innerHTML = `Checkout (<a
+             class="return-to-home-link" href="amazon.html">${displayCartQuantity(cart)} items</a>)`;
+            document.querySelector('.payment-summary-items').innerHTML = `Items (${displayCartQuantity(cart)})`;
+            //console.log(cart);
+
+    });
+});
+
+document.querySelectorAll('.update-quantity-link')
+    .forEach((linkElement) => {
+        linkElement.
+            addEventListener('click', () => {
+                const productId = linkElement.dataset.productId;
+            document.querySelector('.update-quantity-link').style.display = 'none';
+
+                document.querySelector(`.quantity-input-${productId}`).style.display = 'inline';
+                document.querySelector(`.save-quantity-link-${productId}`).style.display = 'inline';
+                document.querySelector(`.save-quantity-link-${productId}`).
+                    addEventListener('click', () => {
+                    updateSave(productId);
+
+                });
+                document.querySelector(`.quantity-input-${productId}`).
+                    addEventListener('keydown', (event) => {
+                        if(event.key === 'Enter') {
+                            updateSave(productId);
+                        }
+                    })
+        })
+    })
+
+document.querySelector('.checkout-header-middle-section').innerHTML = `Checkout (<a class="return-to-home-link"
+ href="amazon.html">${displayCartQuantity(cart)} items</a>)`;
+document.querySelector('.payment-summary-items').innerHTML = `Items (${displayCartQuantity(cart)})`;
+
+function updateSave(productId) {
+    const productQuantity = Number(document.querySelector(`.quantity-input-${productId}`).value);
+    addToCart(productId, productQuantity);
+    saveToCart();
+
+    document.querySelector(`.quantity-label-${productId}`).innerHTML = `${quantityLabel(productId)}`;
+    document.querySelector('.checkout-header-middle-section').innerHTML = `Checkout (<a class="return-to-home-link"href="amazon.html">${displayCartQuantity(cart)} items</a>)`;
+    document.querySelector('.payment-summary-items').innerHTML = `Items (${displayCartQuantity(cart)})`;
+
+    document.querySelector('.update-quantity-link').style.display = 'inline';
+
+    document.querySelector(`.quantity-input-${productId}`).style.display = 'none';
+    document.querySelector(`.save-quantity-link-${productId}`).style.display = 'none';
+}
+
+function quantityLabel(productId) {
+    let matchingProduct;
+
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.id) {
+            matchingProduct = cartItem;
+        }
+    });
+
+    return matchingProduct.quantity;
+
+}
