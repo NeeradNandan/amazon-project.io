6ddd0827387e386b6ab5da3488d3eec401170fd6
Neerad Nandan <nrdnandan1705@gmail.com>
2024-12-28 16:15:45 +0530

    Minor fix:
    - Remove unused variables & modules

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import {addToCart, cart, displayCartQuantity} from "../data/cart.js";
+import {addToCart, displayCartQuantity} from "../data/cart.js";
 import {products} from '../data/products.js';
 import {formatCurrency} from "./utils/money.js";
 
