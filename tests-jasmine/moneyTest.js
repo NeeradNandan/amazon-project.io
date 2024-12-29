5016abf1e6e4de9b749d6351762ea1cb5aaedaa9
Neerad Nandan <nrdnandan1705@gmail.com>
2024-12-29 10:47:52 +0530

    Testing:
    - Remove unnecessary files
    - Use jasmine commands to run tests to be displayed in the webpage
    - Run the test for formatCurrency function

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,20 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
+describe ('Test Suite: formatCurrency', () => {
+    it('Converts cents to dollars', () => {
+        expect(formatCurrency(2095)).toEqual('20.95');
+    })
+
+    it('Converts 0 to 0.00', () => {
+        expect(formatCurrency(0)).toEqual('0.00');
+    })
+    
+    it('Converts 2000.5 to 20.01', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01');
+    })
+    
+    it('Converts 2000.49 to 20.01', () => {
+        expect(formatCurrency(2000.49)).toEqual('20.00');
+    })
+    
+});
