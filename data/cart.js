61a5346fb60970f7140db0b5466670e4f49fa2ae
Neerad Nandan <nrdnandan1705@gmail.com>
2024-12-29 11:58:18 +0530

    Testing:
    - Run the test for addToCart function
    - Restructure the files according to the project dir
    
    Cart restructuring:
    
    - Put the initialization into a function for accessing data from the local storage

--- data/cart.js
+++ data/cart.js
@@ -1,17 +1,23 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
 
-if(!cart) {
-    cart = [
-        {
-            id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-            quantity: 2,
-            deliveryOptionId: '1'
-        },
-        {
-            id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-            quantity: 1,
-            deliveryOptionId: '2'
-        }];
+loadFromStorage();
+
+export function loadFromStorage() {
+    cart = JSON.parse(localStorage.getItem('cart'));
+
+    if (!cart) {
+        cart = [
+            {
+                id: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                id: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+                quantity: 1,
+                deliveryOptionId: '2'
+            }];
+    }
 }
 
 export function addToCart(productId) {
@@ -26,7 +32,8 @@ export function addToCart(productId) {
     const quantitySelector = document.querySelector(`.quantity-selector-${productId}`);
 
     const quantity = Number(quantitySelector.value);
-
+    
+    //console.log(quantity);
     if (matchingItem) {
         matchingItem.quantity += quantity;
     } else {
