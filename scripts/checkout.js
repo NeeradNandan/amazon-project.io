090c6261295377371a0642e5bbb822728d4a9635
Neerad Nandan <nrdnandan1705@gmail.com>
2025-01-05 22:15:34 +0530

    Promises:
    - Use promise instead of callback for better readability and more advanced features

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,9 +4,56 @@ import {renderCheckoutHeader} from "./checkout/checkoutHeader.js";
 import {loadProducts} from "../data/products.js";
 // import '../data/cart-class.js';
 //import '../data/backend-practice.js'
-loadProducts(() => {
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve();
+    })
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+})
+
+/*
+The advantage of promise over callback is that it allows us to chain promises together, instead of nesting callbacks.
+ */
+
+/*
+
+Promise.all([                       It waits for all promises to resolve before executing the next then function
+     new Promise((resolve) => {
+     loadProducts(() => {
+        resolve('value1');
+        })
+     }),
+     new Promise((resolve) => {
+     loadCart(() => {
+        resolve();
+        });
+     })
+   })
+]).then((value) => {
+    console.log(value); // ['value1', undefined]
     renderOrderSummary();
     renderPaymentSummary();
     renderCheckoutHeader();
 });
 
+new Promise((resolve) => {
+    loadProducts(() => {
+        resolve('value1');
+    })
+}).then((value) => {
+    console.log(value); //value1 is displayed which means basically the resolve value of the previous promise is
+                                                    passed to the next then function
+    return new Promise((resolve) => {
+        loadCart(() => {
+            resolve();
+        });
+    })
+}).then(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+    renderCheckoutHeader();
+})
+ */
